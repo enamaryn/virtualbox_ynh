14ec1481785ef35f5cf3be81f632513938316fab
enamaryn <40593656+enamaryn@users.noreply.github.com>
2020-06-07 14:08:16 +0200

    update simple

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,5 @@
-export VBOX_VERSION="5.2.42"
-export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/5.2.42"
+export VBOX_VERSION="5.2"
+export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/5.2.42/"
 export VBOX_EXTPACK="Oracle_VM_VirtualBox_Extension_Pack-5.2.42.vbox-extpack"
 export VBOX_GUEST="VBoxGuestAdditions_5.2.42.iso"
 
