667ce927efe6f76818a121d2c57685d8765d5ada
enamaryn <40593656+enamaryn@users.noreply.github.com>
2021-12-22 15:11:29 +0100

    Update common.sh
    error...

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,4 @@
-export VBOX_VERSION="6.1_6.1.30"
+export VBOX_VERSION="6.1"
 export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/6.1.30"
 export VBOX_EXTPACK="Oracle_VM_VirtualBox_Extension_Pack-6.1.30-148432.vbox-extpack"
 export VBOX_GUEST="VBoxGuestAdditions_6.1.30.iso"
